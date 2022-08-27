aaa007f3eb1f3c139d5bff14606f1940b1569e4b
stopnoanime <m.berecek132@gmail.com>
2022-08-27 22:20:39 +0200

    Add responsive tables

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,5 @@
 const { DateTime } = require("luxon");
+const markdownIt = require("markdown-it");
 
 module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("css");
@@ -7,4 +8,17 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addFilter("toYear", (dateObj) => {
         return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
     });     
+
+    let markdownOptions = {
+        html: true,
+        breaks: true,
+        linkify: true
+    };
+    let markdownLib = new markdownIt(markdownOptions);
+
+    //Add div around tables
+    markdownLib.renderer.rules.table_open = () => '<div class="table-wrapper">\n<table>\n',
+    markdownLib.renderer.rules.table_close = () => '</table>\n</div>',
+
+    eleventyConfig.setLibrary("md", markdownLib);
 };
